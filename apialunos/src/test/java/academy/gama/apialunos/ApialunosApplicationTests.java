8d51ed2e34c832040ade907e02ed58dcd7c99fe4
Jefferson Rodrigues Da Silva <re041598@resource.com.br>
2021-02-22 11:27:53 -0300

    inicio projeto

---
+++ apialunos/src/test/java/academy/gama/apialunos/ApialunosApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.gama.apialunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApialunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
